b932786d9861bb307722d090d1d450fc9b11eed2
William Ayd <william.ayd@icloud.com>
2021-10-29 18:01:20 -0700

    Added non int32 example

--- src/hyperarrow/hyperarrow.cc
+++ src/hyperarrow/hyperarrow.cc
@@ -27,21 +27,24 @@ namespace {
 	  arrow::field("b", arrow::int64()),
 	  arrow::field("c", arrow::int64())
 	});
+
+    arrow::MemoryPool *pool = arrow::default_memory_pool();
+    arrow::Int64Builder int64builder(pool);
+    arrow::Int32Builder int32builder(pool);
     std::shared_ptr<arrow::Array> array_a;
     std::shared_ptr<arrow::Array> array_b;
     std::shared_ptr<arrow::Array> array_c;
-    arrow::NumericBuilder<arrow::Int64Type> builder;
-    ABORT_ON_FAILURE(builder.AppendValues({0, 1, 2, 3, 4, 5, 6, 7, 8, 9}));
-    ABORT_ON_FAILURE(builder.AppendNull());
-    ABORT_ON_FAILURE(builder.Finish(&array_a));
-    builder.Reset();
-    ABORT_ON_FAILURE(builder.AppendValues({9, 8, 7, 6, 5, 4, 3, 2, 1, 0}));
-    ABORT_ON_FAILURE(builder.AppendNull());
-    ABORT_ON_FAILURE(builder.Finish(&array_b));
-    builder.Reset();
-    ABORT_ON_FAILURE(builder.AppendValues({1, 2, 1, 2, 1, 2, 1, 2, 1, 2}));
-    ABORT_ON_FAILURE(builder.AppendNull());
-    ABORT_ON_FAILURE(builder.Finish(&array_c));
+    ABORT_ON_FAILURE(int64builder.AppendValues({0, 1, 2, 3, 4, 5, 6, 7, 8, 9}));
+    ABORT_ON_FAILURE(int64builder.AppendNull());
+    ABORT_ON_FAILURE(int64builder.Finish(&array_a));
+    int64builder.Reset();
+    ABORT_ON_FAILURE(int32builder.AppendValues({9, 8, 7, 6, 5, 4, 3, 2, 1, 0}));
+    ABORT_ON_FAILURE(int32builder.AppendNull());
+    ABORT_ON_FAILURE(int32builder.Finish(&array_b));
+    int32builder.Reset();
+    ABORT_ON_FAILURE(int64builder.AppendValues({1, 2, 1, 2, 1, 2, 1, 2, 1, 2}));
+    ABORT_ON_FAILURE(int64builder.AppendNull());
+    ABORT_ON_FAILURE(int64builder.Finish(&array_c));
     return arrow::Table::Make(schema, {array_a, array_b, array_c});
   }
 
