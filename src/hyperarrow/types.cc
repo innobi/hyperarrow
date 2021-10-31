7e16e8d2f874851f1488c1d8228e5b9721416ce1
William Ayd <william.ayd@icloud.com>
2021-10-31 11:01:54 -0700

    more types

--- src/hyperarrow/types.cc
+++ src/hyperarrow/types.cc
@@ -6,7 +6,22 @@ namespace hyperarrow {
       return hyperapi::SqlType::smallInt();
     } else if (arrowType == arrow::int32())  {
       return  hyperapi::SqlType::integer();
-    } else if (arrowType == arrow::int64())
+    } else if (arrowType == arrow::int64()) {
       return hyperapi::SqlType::bigInt();
+    } else if (arrowType == arrow::float16()) {
+      return hyperapi::SqlType::doublePrecision();      
+    } else if (arrowType == arrow::float32()) {
+      return hyperapi::SqlType::doublePrecision();
+    } else if (arrowType == arrow::float64()) {
+      return hyperapi::SqlType::doublePrecision();
+    } else if (arrowType == arrow::boolean()) {
+      return hyperapi::SqlType::boolean();
+    } else if (arrowType == arrow::utf8()) {
+      return hyperapi::SqlType::text();
+    } else if (arrowType == arrow::date32()) {
+      return hyperapi::SqlType::date();
+    } else if (arrowType == arrow::date64()) {
+      return hyperapi::SqlType::date();
+    }
   }
 }
