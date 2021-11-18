83547bfafcba1e9cd35b666be0a48e4da8e2c863
Will Ayd <willayd@Wills-iMac.attlocal.net>
2021-11-17 18:32:53 -0800

    shell for reader impl

---
+++ include/hyperarrow/reader.h
@@ -0,0 +1,5 @@
+#include <arrow/table.h>
+
+namespace hyperarrow {
+  std::shared_ptr<arrow::Table> arrowTableFromHyper(const std::string path);
+}
---
+++ src/reader.cc
@@ -0,0 +1,33 @@
+#include <hyperarrow/reader.h>
+
+#include <arrow/builder.h>
+#include <arrow/table.h>
+
+#include <hyperarpi/hyperapi.hpp>
+
+#include <map>
+#include "types.h"
+
+static std::shared_ptr<arrow::Schema> arrowSchemaFromHyperTableDef(hyperapi::TableDefinition tableDefinition) { }
+
+static std::shared_ptr<arrow::Table> arrowTableFromHyperResult(hyperapi::Result result) {
+  for (const hyperapi::Row& row : result) {
+    for (const hyperapi::Value& value : row ) {
+      // TODO: add to arrow table builders
+    }
+  }
+}
+
+namespace hyperarrow {
+  std::shared_ptr<arrow::Table> arrowTableFromHyper(const std::string path) {
+    hyperapi::HyperProcess hyper(hyperapi::Telemetry::DoNotSendUsageDataToTableau);
+    {
+      hyperapi::Connection connection(hyper.getEndpoint(), path);
+      const hyperapi::Catalog& catalog = connection.getCatalog();
+      hyperapi::TableName extractTable("Extract", "Extract");
+
+      hyperapi::TableDefinition tableDefinition = catalog.getTableDefinition(extractTable);
+      hyperapi::Result rowsInTable = connection.executeQuery("SELECT * FROM " + extractTable.ToString());
+      
+  }
+}
