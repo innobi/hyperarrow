cadf89aa50b537ee3deafa9011ee23c9a4edbeab
William Ayd <william.ayd@icloud.com>
2021-11-27 12:06:50 -0700

    testing stubs

---
+++ tests/test_writer.cc
@@ -0,0 +1,84 @@
+#define BOOST_TEST_MODULE hyperarrow_writer_tests
+#include <boost/filesystem.hpp>
+#include <boost/test/included/unit_test.hpp>
+#include <hyperarrow/writer.h>
+#include <arrow/api.h>
+
+#define ABORT_ON_FAILURE(expr)                     \
+  do {                                             \
+    arrow::Status status_ = (expr);                \
+    if (!status_.ok()) {                           \
+      std::cerr << status_.message() << std::endl; \
+      abort();                                     \
+    }                                              \
+  } while (0);
+
+
+BOOST_AUTO_TEST_CASE(test_basic_write) {
+  auto schema = arrow::schema(
+      {arrow::field("a", arrow::int16()), arrow::field("b", arrow::int32()),
+       arrow::field("c", arrow::int64()), arrow::field("d", arrow::float32()),
+       arrow::field("e", arrow::float64()), arrow::field("f", arrow::boolean()),
+       arrow::field("g", arrow::date32()), arrow::field("h", arrow::utf8())});
+
+  arrow::MemoryPool* pool = arrow::default_memory_pool();
+  arrow::Int16Builder int16builder(pool);
+  arrow::Int32Builder int32builder(pool);
+  arrow::Int64Builder int64builder(pool);
+  arrow::FloatBuilder floatbuilder(pool);
+  arrow::DoubleBuilder doublebuilder(pool);
+  arrow::BooleanBuilder boolbuilder(pool);
+  arrow::StringBuilder stringbuilder(pool);
+  arrow::Date32Builder date32builder(pool);
+  std::shared_ptr<arrow::Array> array_a;
+  std::shared_ptr<arrow::Array> array_b;
+  std::shared_ptr<arrow::Array> array_c;
+  std::shared_ptr<arrow::Array> array_d;
+  std::shared_ptr<arrow::Array> array_e;
+  std::shared_ptr<arrow::Array> array_f;
+  std::shared_ptr<arrow::Array> array_g;
+  std::shared_ptr<arrow::Array> array_h;
+
+  ABORT_ON_FAILURE(int16builder.AppendValues({0, 1, 2, 3, 4, 5, 6, 7, 8, 9}));
+  ABORT_ON_FAILURE(int16builder.AppendNull());
+  ABORT_ON_FAILURE(int16builder.Finish(&array_a));
+
+  ABORT_ON_FAILURE(int32builder.AppendValues({9, 8, 7, 6, 5, 4, 3, 2, 1, 0}));
+  ABORT_ON_FAILURE(int32builder.AppendNull());
+  ABORT_ON_FAILURE(int32builder.Finish(&array_b));
+
+  ABORT_ON_FAILURE(int64builder.AppendValues({1, 2, 1, 2, 1, 2, 1, 2, 1, 2}));
+  ABORT_ON_FAILURE(int64builder.AppendNull());
+  ABORT_ON_FAILURE(int64builder.Finish(&array_c));
+
+  ABORT_ON_FAILURE(
+      floatbuilder.AppendValues({0., 1., 2., 3., 4., 5., 6., 7., 8., 9.}));
+  ABORT_ON_FAILURE(floatbuilder.AppendNull());
+  ABORT_ON_FAILURE(floatbuilder.Finish(&array_d));
+
+  ABORT_ON_FAILURE(
+      doublebuilder.AppendValues({0., 1., 2., 3., 4., 5., 6., 7., 8., 9.}));
+  ABORT_ON_FAILURE(doublebuilder.AppendNull());
+  ABORT_ON_FAILURE(doublebuilder.Finish(&array_e));
+
+  ABORT_ON_FAILURE(boolbuilder.AppendValues(std::vector<bool>{
+      true, false, true, false, true, false, true, false, true, false}));
+  ABORT_ON_FAILURE(boolbuilder.AppendNull());
+  ABORT_ON_FAILURE(boolbuilder.Finish(&array_f));
+
+  ABORT_ON_FAILURE(date32builder.AppendValues({0, 1, 2, 3, 4, 5, 6, 7, 8, 9}));
+  ABORT_ON_FAILURE(date32builder.AppendNull());
+  ABORT_ON_FAILURE(date32builder.Finish(&array_g));
+
+  ABORT_ON_FAILURE(stringbuilder.AppendValues(
+      {"a", "b", "c", "d", "e", "f", "g", "h", "i", "j"}));
+  ABORT_ON_FAILURE(stringbuilder.AppendNull());
+  ABORT_ON_FAILURE(stringbuilder.Finish(&array_h));
+
+  auto table = arrow::Table::Make(schema, {array_a, array_b, array_c, array_d,
+                                     array_e, array_f, array_g, array_h});
+
+  const std::string path = "example.hyper";
+  hyperarrow::arrowTableToHyper(table, path, "schema", "table");
+  BOOST_TEST(boost::filesystem::exists(path));
+}
