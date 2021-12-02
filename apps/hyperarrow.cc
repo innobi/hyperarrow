95f1e98fa7cdd1bf0b67ba0e124e77a4a42299de
William Ayd <william.ayd@icloud.com>
2021-12-01 17:44:48 -0700

    change app to generate test data

--- apps/hyperarrow.cc
+++ apps/hyperarrow.cc
@@ -24,7 +24,8 @@ std::shared_ptr<arrow::Table> createTable() {
       {arrow::field("a", arrow::int16()), arrow::field("b", arrow::int32()),
        arrow::field("c", arrow::int64()), arrow::field("d", arrow::float32()),
        arrow::field("e", arrow::float64()), arrow::field("f", arrow::boolean()),
-       arrow::field("g", arrow::date32()), arrow::field("h", arrow::utf8())});
+       arrow::field("g", arrow::date32()), arrow::field("h", arrow::utf8()),
+       arrow::field("i", arrow::timestamp(arrow::TimeUnit::MICRO))});
 
   arrow::MemoryPool *pool = arrow::default_memory_pool();
   arrow::Int16Builder int16builder(pool);
@@ -35,6 +36,7 @@ std::shared_ptr<arrow::Table> createTable() {
   arrow::BooleanBuilder boolbuilder(pool);
   arrow::StringBuilder stringbuilder(pool);
   arrow::Date32Builder date32builder(pool);
+  arrow::TimestampBuilder tsbuilder(arrow::timestamp(arrow::TimeUnit::MICRO), pool);
   std::shared_ptr<arrow::Array> array_a;
   std::shared_ptr<arrow::Array> array_b;
   std::shared_ptr<arrow::Array> array_c;
@@ -43,45 +45,48 @@ std::shared_ptr<arrow::Table> createTable() {
   std::shared_ptr<arrow::Array> array_f;
   std::shared_ptr<arrow::Array> array_g;
   std::shared_ptr<arrow::Array> array_h;
+  std::shared_ptr<arrow::Array> array_i;
 
-  ABORT_ON_FAILURE(int16builder.AppendValues({0, 1, 2, 3, 4, 5, 6, 7, 8, 9}));
+  ABORT_ON_FAILURE(int16builder.Append(16));
   ABORT_ON_FAILURE(int16builder.AppendNull());
   ABORT_ON_FAILURE(int16builder.Finish(&array_a));
 
-  ABORT_ON_FAILURE(int32builder.AppendValues({9, 8, 7, 6, 5, 4, 3, 2, 1, 0}));
+  ABORT_ON_FAILURE(int32builder.Append(32));
   ABORT_ON_FAILURE(int32builder.AppendNull());
   ABORT_ON_FAILURE(int32builder.Finish(&array_b));
 
-  ABORT_ON_FAILURE(int64builder.AppendValues({1, 2, 1, 2, 1, 2, 1, 2, 1, 2}));
+  ABORT_ON_FAILURE(int64builder.Append(64));
   ABORT_ON_FAILURE(int64builder.AppendNull());
   ABORT_ON_FAILURE(int64builder.Finish(&array_c));
 
   ABORT_ON_FAILURE(
-      floatbuilder.AppendValues({0., 1., 2., 3., 4., 5., 6., 7., 8., 9.}));
+		   floatbuilder.Append(1.234));
   ABORT_ON_FAILURE(floatbuilder.AppendNull());
   ABORT_ON_FAILURE(floatbuilder.Finish(&array_d));
 
   ABORT_ON_FAILURE(
-      doublebuilder.AppendValues({0., 1., 2., 3., 4., 5., 6., 7., 8., 9.}));
+		   doublebuilder.Append(123.4));
   ABORT_ON_FAILURE(doublebuilder.AppendNull());
   ABORT_ON_FAILURE(doublebuilder.Finish(&array_e));
 
-  ABORT_ON_FAILURE(boolbuilder.AppendValues(std::vector<bool>{
-      true, false, true, false, true, false, true, false, true, false}));
+  ABORT_ON_FAILURE(boolbuilder.Append(true));
   ABORT_ON_FAILURE(boolbuilder.AppendNull());
   ABORT_ON_FAILURE(boolbuilder.Finish(&array_f));
 
-  ABORT_ON_FAILURE(date32builder.AppendValues({0, 1, 2, 3, 4, 5, 6, 7, 8, 9}));
+  ABORT_ON_FAILURE(date32builder.Append(0));
   ABORT_ON_FAILURE(date32builder.AppendNull());
   ABORT_ON_FAILURE(date32builder.Finish(&array_g));
 
-  ABORT_ON_FAILURE(stringbuilder.AppendValues(
-      {"a", "b", "c", "d", "e", "f", "g", "h", "i", "j"}));
+  ABORT_ON_FAILURE(stringbuilder.Append("a"));
   ABORT_ON_FAILURE(stringbuilder.AppendNull());
   ABORT_ON_FAILURE(stringbuilder.Finish(&array_h));
 
+  ABORT_ON_FAILURE(tsbuilder.Append(0));
+  ABORT_ON_FAILURE(tsbuilder.AppendNull());
+  ABORT_ON_FAILURE(tsbuilder.Finish(&array_i));  
+
   return arrow::Table::Make(schema, {array_a, array_b, array_c, array_d,
-                                     array_e, array_f, array_g, array_h});
+                                     array_e, array_f, array_g, array_h, array_i});
 }
 
 Status RunMain(int argc, char **argv) {
