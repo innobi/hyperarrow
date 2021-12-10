9f1a82e098169a6fd8b7b2b91ba4f571b7d072e8
William Ayd <william.ayd@icloud.com>
2021-12-09 23:14:30 -0800

    try 10.9 macos target

--- python/setup.py
+++ python/setup.py
@@ -31,7 +31,7 @@ if "MACOSX_DEPLOYMENT_TARGET" not in os.environ:
     python_target = sysconfig.get_config_vars().get(
         "MACOSX_DEPLOYMENT_TARGET", current_system
     )
-    target_macos_version = "11.0"
+    target_macos_version = "10.9"
     parsed_macos_version = pkg_resources.parse_version(target_macos_version)
     if (
         pkg_resources.parse_version(str(python_target)) < parsed_macos_version
