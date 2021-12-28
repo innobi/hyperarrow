f3a4386c5a91ab6c4775c2ff18fb5a1566d0d485
William Ayd <william.ayd@icloud.com>
2021-12-28 11:05:06 -0500

    different search path

--- python/setup.py
+++ python/setup.py
@@ -34,7 +34,7 @@ if not tableau_dir:
 
 extra_link_args = []
 if sys.platform == "darwin":
-    extra_link_args = ["-rpath", f"/usr/local/lib/:{tableau_dir}/lib"]
+    extra_link_args = ["-rpath", "/usr/local/lib/", "-rpath", f"{tableau_dir}/lib"]
 
 hyperarrow_module = Extension(
     "hyperarrow.libhyperarrow",
