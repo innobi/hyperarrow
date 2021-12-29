ecbb7de13cd724d5633b03f5d74789f70a7770d2
William Ayd <will_ayd@innobi.io>
2021-12-29 16:53:30 -0500

    Format cleanup (#23)

--- python/hyperarrow/tests/test_exceptions.py
+++ python/hyperarrow/tests/test_exceptions.py
@@ -5,7 +5,6 @@ import hyperarrow as hal
 
 
 def test_unsupported_type(tmp_hyper):
-    tbl = pa.Table.from_arrays([[None]], names=['a'])
+    tbl = pa.Table.from_arrays([[None]], names=["a"])
     with pytest.raises(RuntimeError, match="Type not supported"):
         hal.write_to_hyper(tbl, str(tmp_hyper), "schema", "table")
-    
