4c0842f86a0fef1a7332a5a5efa837fff7ae6b21
Will Ayd <william.ayd@icloud.com>
2022-01-13 20:10:21 -0800

    fixed script errors

--- ci/scripts/install_arrow.bat
+++ ci/scripts/install_arrow.bat
@@ -1,7 +1,10 @@
 mkdir C:\tmp
 pushd C:\tmp
 git clone https://github.com/Microsoft/vcpkg.git
-.\vcpkg\bootstrap-vcpkg.bat
+echo "Bootstrapping vcpkg"
+call .\vcpkg\bootstrap-vcpkg.bat
+echo "Running Arrow Install"
 .\vcpkg\vcpkg install arrow:x64-windows
+echo "Integrating vcpkg with Visual Studio"
 .\vcpkg\vcpkg integrate install
 popd
